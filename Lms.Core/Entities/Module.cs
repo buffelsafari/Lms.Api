997ab9f6b8ffd2cf9a3b5b3a9ab151176e39400c
buffelsafari <buffelsafari@hotmail.com>
2021-09-28 16:34:14 +0200

    Add project files.

---
+++ Lms.Core/Entities/Module.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lms.Core.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public int courseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Lms.Data/Data/LmsDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lms.Core.Entities;
+
+namespace Lms.Data.Data
+{
+    public class LmsDataContext : DbContext
+    {
+        public DbSet<Lms.Core.Entities.Course> Course { get; set; }
+        public DbSet<Lms.Core.Entities.Module> Module { get; set; }
+
+
+        public LmsDataContext (DbContextOptions<LmsDataContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
