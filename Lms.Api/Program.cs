fddbfa0fbf1b803bd1259c88a1461b493d18ce27
buffelsafari <buffelsafari@hotmail.com>
2021-09-29 08:57:38 +0200

    seed

--- Lms.Api/Program.cs
+++ Lms.Api/Program.cs
@@ -1,5 +1,8 @@
+using Lms.Data.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,26 @@ namespace Lms.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<LmsDataContext>();
+
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
+                try 
+                {
+                    SeedData.InitAsync(context, services).Wait();
+                } 
+                catch(Exception e) 
+                {
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Lms.Data/Data/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Bogus;
+using Lms.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data
+{
+    public class SeedData
+    {
+
+        public static async Task InitAsync(LmsDataContext context, IServiceProvider services)
+        {
+            Faker faker = new Faker("sv");
+
+            List<Course> courses = new List<Course>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                List<Module> modules = new List<Module>();
+                for (int j = 0; j < faker.Random.Int(1, 5); j++)
+                {
+                    modules.Add(new Module
+                    {
+                        Title = faker.Commerce.ProductAdjective(),
+                        StartDate = DateTime.Now.AddDays(faker.Random.Int(-3, 3))
+
+                    });
+                }
+
+                courses.Add(new Course
+                {
+                    Title = faker.Commerce.ProductName(),
+                    StartDate = DateTime.Now.AddDays(faker.Random.Int(-3, 3)),
+                    Modules = modules
+
+                });
+
+            }
+
+            context.AddRange(courses);
+            await context.SaveChangesAsync();
+
+        }
+
+
+    }
+}
